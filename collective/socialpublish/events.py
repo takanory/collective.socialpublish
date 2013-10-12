4de8e782546d55ba416608daa40ad83531264b5f
Takanori Suzuki <takanori@takanory.net>
2013-10-12 13:31:33 +0900

    add tweet method

--- collective/socialpublish/events.py
+++ collective/socialpublish/events.py
@@ -4,9 +4,16 @@ from zope.component import getUtility
 from plone.registry.interfaces import IRegistry
 from collective.socialpublish.controlpanel.interfaces import ISocialPublishControlPanel
 
-def _dummy_tw_push(*args):
-    print args #TODO
-    pass
+import tweepy
+
+def tw_push(tw_consumer_key, tw_consumer_secret, tw_access_key,
+            tw_access_secret, message):
+    # TODO: rename _key to _token
+    auth = tweepy.OAuthHandler(tw_consumer_key, tw_consumer_secret)
+    auth.set_access_token(tw_access_key, access_token_secret)
+    api = tweepy.API(auth_handler=auth, api_root='/1.1', secure=True)
+
+    api.update_status(message)
 
 def _dummy_fb_pash(*args):
     print args #TODO
@@ -35,9 +42,9 @@ def social_publish(obj, event):
     message = prefix_message + obj.title + u" " + obj.absolute_url()
     if tw_consumer_key and tw_consumer_secret and \
             tw_access_key and tw_access_secret:
-        _dummy_tw_push(tw_consumer_key, tw_consumer_secret,
-                       tw_access_key, tw_access_secret,
-                       message)
+       tw_push(tw_consumer_key, tw_consumer_secret,
+               tw_access_key, tw_access_secret,
+               message)
     if fb_app_id and fb_app_secret and fb_user_id:
         _dummy_fb_pash(fb_app_id, fb_app_secret, fb_user_id,
                        message)
