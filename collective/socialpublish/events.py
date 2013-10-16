d253f5b1bcaac716c7e2b15c4d317a1517959f5e
Takanori Suzuki <takanori@takanory.net>
2013-10-16 20:40:46 +0900

    fix Facebook push

--- collective/socialpublish/events.py
+++ collective/socialpublish/events.py
@@ -1,6 +1,7 @@
 #
 
 import urllib
+from urlparse import urlparse
 
 from zope.component import getUtility
 from plone.registry.interfaces import IRegistry
@@ -9,32 +10,44 @@ from collective.socialpublish.controlpanel.interfaces import ISocialPublishContr
 import tweepy
 from facebook import GraphAPI
 
+# Endpointo of Facebook Graph API
 ENDPOINT = 'graph.facebook.com'
 
 def tw_push(tw_consumer_token, tw_consumer_secret, tw_access_token,
-            tw_access_secret, message):
+            tw_access_secret, message, url):
     auth = tweepy.OAuthHandler(tw_consumer_token, tw_consumer_secret)
     auth.set_access_token(tw_access_token, tw_access_secret)
     api = tweepy.API(auth_handler=auth, api_root='/1.1', secure=True)
 
-    api.update_status(message)
+    api.update_status(message + u" " + url)
 
-def fb_push(fb_app_id, fb_app_secret, fb_user_id, message):
+def fb_push(fb_app_id, fb_app_secret, fb_user_id, message, url):
     oauth_args = dict(client_id     = fb_app_id,
                       client_secret = fb_app_secret,
                       grant_type    = 'client_credentials')
     oauth_param = urllib.urlencode(oauth_args)
     oauth_url = 'https://' + ENDPOINT + '/oauth/access_token?' + oauth_param
     oauth_response = urllib.urlopen(oauth_url).read()
-    _token, access_token = oauth_response.split('=')
+    _param, access_token = oauth_response.split('=')
+
+    url = 'http://plone.jp/news/ploneSeminar40'
+    o = urlparse(url)
+    if "." not in o.netloc:
+        # localhost(e.g. http://localhost:8080) can not use link paramater
+        message = message + u" " + url
+        data = {
+            'message': message.encode('utf-8'),
+            'privacy': {'value':'SELF'}, # for testing
+            }
+    else:
+        data = {
+            'message': message.encode('utf-8'),
+            'link': url,
+            'privacy': {'value':'SELF'}, # for testing
+            }
 
     graph = GraphAPI(access_token)
-    graph.put_wall_post(message=message)
-    #graph.put_object("me", # self wall
-    #                 "feed",
-    #                 message=message
-    #                 privacy={'value':'SELF'},
-    #                 )
+    graph.put_object(fb_user_id, 'feed', **data)
 
 def social_publish(obj, event):
     registry = getUtility(IRegistry)
@@ -42,6 +55,8 @@ def social_publish(obj, event):
     content_types = settings.content_types
     workflow_transitions = settings.workflow_transitions
     if obj.portal_type not in content_types:
+        return None
+    if event.transition == None:
         return None
     if event.transition.id not in workflow_transitions:
         return None
@@ -55,11 +70,12 @@ def social_publish(obj, event):
     fb_app_secret = settings.fb_app_secret
     fb_user_id = settings.fb_user_id
 
-    message = prefix_message + obj.title + u" " + obj.absolute_url()
+    message = prefix_message + obj.title
+    url = obj.absolute_url()
     if tw_consumer_token and tw_consumer_secret and \
             tw_access_token and tw_access_secret:
        tw_push(tw_consumer_token, tw_consumer_secret,
                tw_access_token, tw_access_secret,
-               message)
+               message, url)
     if fb_app_id and fb_app_secret and fb_user_id:
-        fb_push(fb_app_id, fb_app_secret, fb_user_id, message)
+        fb_push(fb_app_id, fb_app_secret, fb_user_id, message, url)
